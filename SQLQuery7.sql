7343b8d9c6b0f67fa7b8a37c086a0b8a9b961d53
saloxibragimov55-pixel <saloxibragimov55@gmail.com>
2025-11-08 15:42:43 +0500

    Add files via upload

---
+++ SQLQuery7.sql
@@ -0,0 +1,43 @@
+Create table salary(
+empid int not null foreign key references mentors (id),
+salary money
+
+
+)
+
+alter table salary add constraint CHECK_Salary_Salary
+CHECK(Salary>400000)
+
+alter table mentors
+Drop column id
+alter table mentors
+drop column id
+alter table mentors
+add mentroid
+Int Identity (1,1)
+
+select * from mentors
+alter table mentors
+add name nvarchar(50)
+insert into mentors(id, name)
+Values (120,'Salohiddin')
+select * from mentors
+SET IDENTITY_INSERT mentors ON;
+Insert Into mentors (id, mentroid, name)
+Values (2 ,100,'Mentor')
+
+select * from mentors
+
+Insert into mentors(id, name)
+Values (12,'Sonya')
+
+create table books (
+id INT IDENTITY (1000,10),
+name nvarchar(max)
+default 'Bookname',
+Author nvarchar(max)
+)
+select * from Books
+insert into books(name, Author)
+Values ('Metro 2033', 'Dmitriy')
+SET IDENTITY_INSERT Books ON;
